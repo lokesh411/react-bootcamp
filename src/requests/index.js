413a21518db527967be290b2cb91c7afefc4e1ec
lokesh <lokeshwaran@yellowmessenger.com>
2022-02-06 11:15:09 +0530

    changes in backend url

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,6 +1,7 @@
+const BACKEND_URL = "https://express-bootcamp.herokuapp.com"
 export const fetchAllUsers = async () => {
     console.log("in fetch all users ")
-    const response = await fetch('/api/user')
+    const response = await fetch(`${BACKEND_URL}/api/user`)
     const data = await response.json()
     return data;
 }
